35e3a4bece40f7099dbd424b5ec16ebd590b18fb
ijhar <ijharalkawsary@gmail.com>
2017-06-04 21:48:02 +0600

    Username Field in forms

--- account/forms.py
+++ account/forms.py
@@ -11,7 +11,7 @@ class CustomUserCreationForm(UserCreationForm):
 
     def __init__(self, *args, **kargs):
         super(CustomUserCreationForm, self).__init__(*args, **kargs)
-        del self.fields['username']
+        #del self.fields['username']
 
     class Meta:
         model = User
@@ -26,7 +26,7 @@ class CustomUserChangeForm(UserChangeForm):
 
     def __init__(self, *args, **kargs):
         super(CustomUserChangeForm, self).__init__(*args, **kargs)
-        del self.fields['username']
+        #del self.fields['username']
 
     class Meta:
         model = User
