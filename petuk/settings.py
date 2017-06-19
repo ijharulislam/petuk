fedd8e7601e684eee5eb8d2bb96daf925e5e00e7
ijhar <ijharalkawsary@gmail.com>
2017-06-20 03:43:29 +0600

    Static ROOT

--- petuk/settings.py
+++ petuk/settings.py
@@ -170,14 +170,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'staticfiles_env', 'static_root')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static', 'assets'),
 
-STATICFILES_DIRS = [
-    os.path.join(REPO_DIR, "assets"),
-]
-
-STATIC_ROOT = os.path.join(BASE_DIR, "..", "www", "static")
-STATIC_URL = '/static/'
-
+)
 
 
 # REST FRAMEWORK
