8bc36e8f5d105058e999f4377a4d141a0563966d
ijhar <ijharalkawsary@gmail.com>
2017-06-20 03:52:33 +0600

    Serving Static Files

--- petuk/urls.py
+++ petuk/urls.py
@@ -13,8 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf.urls.static import static
 
 from .api import api
 
@@ -23,3 +25,6 @@ urlpatterns = [
     url(r'^api/', include(api.urls)),
     url(r'^admin/', admin.site.urls),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
