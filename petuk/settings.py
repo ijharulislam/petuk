52d1e05509ddc2a84a265460a6eb49e8a7a4a495
ijhar <ijharalkawsary@gmail.com>
2017-06-20 16:42:18 +0600

    static files

--- petuk/settings.py
+++ petuk/settings.py
@@ -170,11 +170,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'staticfiles_env', 'static_root')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-
-)
+STATIC_ROOT = os.path.join(BASE_DIR, "..", "www", "static")
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'staticfiles_env', 'media_root',)
