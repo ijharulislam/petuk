096ca558234b97fa9ba0a4dd7e1cf4795ce781a1
ijhar <ijharalkawsary@gmail.com>
2017-06-21 22:16:15 +0600

    Offer serializer Changed

--- offer/serializers.py
+++ offer/serializers.py
@@ -1,8 +1,12 @@
 from rest_framework.serializers import ModelSerializer
 from .models import Offer
+from restaurant.serializers import RestaurantSerializer, ItemSerializer
 
 
 class OfferSerializer(ModelSerializer):
+    restaurant = RestaurantSerializer(read_only=True, many=True)
+    item = ItemSerializer(read_only=True, many=True)
+
     class Meta:
         model = Offer
         fields = '__all__'
